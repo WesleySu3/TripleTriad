699d7451f765b59c8511c561f5ae5f7d22e1f634
WesleySu3 <wesleysu3@gmail.com>
2021-06-29 17:35:14 -0700

    Added menu to randomize decks

---
+++ ElementGUI.java
@@ -0,0 +1,26 @@
+public class ElementGUI extends GUI {
+
+	public ElementGUI() {
+
+		/*************************
+		|  index  |    box       |
+		|------------------------|
+		|    0    |  None    	 |
+		|    1    |  Earth       |
+		|    2    |  Fire        |
+		|    3    |  Water       |
+		|    4    |  Poison      |
+		|    5    |  Holy        |
+		|	 6	  |  Lightning   |
+		|	 7	  |  Wind        |
+		|	 8    |  Ice         |
+		|    9    |  back button |
+		**************************/
+
+
+
+	}
+
+	public void draw() {
+	}
+}
